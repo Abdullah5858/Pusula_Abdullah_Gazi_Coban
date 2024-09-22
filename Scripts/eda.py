cdb01397316afabbbcd4b897e109d12c20ae2863
Abdullah <102368976+Abdullah5858@users.noreply.github.com>
2024-09-22 21:12:10 +0300

    Create eda.py

---
+++ Scripts/eda.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path = r"C:\Users\coban\Desktop\ornek_proje\side_effect_data 1.xlsx"
+df = pd.read_excel("side_effect_data 1.xlsx")
+df
+df.head()
+df.info()
